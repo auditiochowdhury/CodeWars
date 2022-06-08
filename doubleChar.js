231325a9938fd5eb54d9f9e3e72d196f7ee38255
auditiochowdhury <auditioc@gmail.com>
2022-06-08 16:21:39 -0400

    Function to return a string where each string is repeated once

---
+++ doubleChar.js
@@ -0,0 +1,9 @@
+//Given a string, you have to return a string in which each character (case-sensitive) is repeated once.
+
+
+function doubleChar(str) {
+    return [...str].map(s => s.repeat(2)).join('')
+  
+    // Your code here
+  }
+  
